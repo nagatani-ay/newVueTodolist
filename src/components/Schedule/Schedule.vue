682e74ef1414b9c433edf740dfbbd4b7c2023d9b
nagatani-ay <nagatani@ay-sys.com>
2022-05-20 10:17:48 +0900

    1 file modified

--- src/components/Schedule/Schedule.vue
+++ src/components/Schedule/Schedule.vue
@@ -1,5 +1,47 @@
-<template></template>
+<template>
+  <div class="Schedule">
+    <div class="calender__table" v-for="day in getDays()">{{ day }}</div>
+  </div>
+</template>
 <script>
-export default {};
+import { ref, onMounted } from 'vue';
+export default {
+  props: ['todoList'],
+  setup(props) {
+    let ToDay = ref('test');
+    let days = ref(0);
+    function getDays() {
+      return days;
+    }
+    function createCalender(year, month) {
+      return new Date(parseInt(year, 10), parseInt(month + 1, 10), 0).getDate();
+    }
+    onMounted(() => {
+      const today = new Date();
+      days = createCalender(today.getFullYear(), today.getMonth());
+    });
+    return {
+      ToDay,
+      days,
+      getDays,
+      createCalender,
+    };
+  },
+};
 </script>
-<style></style>
+<style>
+.Schedule {
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  align-items: center;
+}
+.calender__table {
+  display: flex;
+  width: 14%;
+  height: auto;
+}
+.calendar__day {
+  display: flex;
+}
+</style>
